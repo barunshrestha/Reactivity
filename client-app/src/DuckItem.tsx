d50141345d00e2df528712eacc3da42576a2c465
Barun Shrestha <BShrestha@mdot.state.md.us>
2024-07-23 20:44:29 -0400

    end of stage 2

---
+++ client-app/src/DuckItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Duck } from './demo'
+
+interface Props {
+    duck: Duck
+}
+
+function DuckItem({duck}: Props) {
+  return (
+    <div key={duck.name}>
+    <span>{duck.name}</span>
+    <button onClick={() => duck.makeSound(duck.name + 'quack')}>Make Sound</button>
+</div>
+  )
+}
+
+export default DuckItem
